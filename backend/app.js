8aea11bbac84383ea1e1153198d76e2f8fa15538
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-10-30 18:13:57 -0300

    first part 5 commit

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+// use express-async-errors instead of try/catch
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.getToken)
+
+app.use('/api/blogs', middleware.getUser, blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.errorHandler)
+
+module.exports = app
