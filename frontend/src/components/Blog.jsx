8aae0a758194f0ed295cf8c124c910a04aa99f6c
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-11-04 14:45:01 -0300

    add exercise 5.7

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-	<div>
-		{blog.title} {blog.author}
-	</div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+	const [visible, setVisible] = useState(false)
+	const [buttonContent, setButtonContent] = useState("view")
+
+	const handleVisibility = () => {
+		setVisible(!visible)
+		if(buttonContent === "view") {
+			setButtonContent("hide")
+		} else {
+			setButtonContent("view")
+		}
+		
+	}
+
+	const showWhenVisible = { display: visible ? "" : "none" }
+
+	return (
+		<div className="blogStyle">
+			<span>
+				{blog.title} - {blog.author} 
+				<button className="smallButton" onClick={handleVisibility}>{buttonContent}</button>
+			</span>
+			<div style={showWhenVisible}>
+				<div>{blog.url}</div>
+				<div>{blog.likes} <button>like</button></div>
+				<div>{blog.user.name}</div>
+			</div>
+
+		</div>  
+	)
+
+}
 
 export default Blog
